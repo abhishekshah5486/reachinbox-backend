a817bfd451bd73ed3fe058eb49c09ab46b2569c0
abhishekshah5486 <abhishek.shah5486@gmail.com>
2025-03-05 14:43:54 +0530

    controllers to filter emails by account and folder in es

--- controllers/elasticSearchController.js
+++ controllers/elasticSearchController.js
@@ -1,4 +1,4 @@
-const { searchEmailsByQuery, searchEmailsByDateRange } = require('../services/elasticService');
+const { searchEmailsByQuery, searchEmailsByDateRange,  filterEmailsByFolderAndAccount, filterEmailsByAccount, filterEmailsByFolder} = require('../services/elasticService');
 
 exports.searchEmailsByQuery = async (req, res) => {
     try {
@@ -39,6 +39,78 @@ exports.searchEmailsByDateRange = async (req, res) => {
         return res.status(500).json({
             success: false,
             message: "Error searching emails. Please try again later.",
+            error: error.message
+        });
+        
+    }
+}
+
+exports.filterEmailsByFolderAndAccount = async (req, res) => {
+    try {
+        
+        const { folder, email } = req.query;
+        const { userId } = req.params;
+
+        const searchResults = await filterEmailsByFolderAndAccount(userId, folder, email);
+        res.status(200).json({
+            success: true,
+            message: "Filtered emails retrieved.",
+            searchResults: searchResults
+        })
+
+    } catch (error) {
+
+        return res.status(500).json({
+            success: false,
+            message: "Error filtering emails. Please try again later.",
+            error: error.message
+        });
+        
+    }
+}
+
+exports.filterEmailsByAccount = async (req, res) => {
+    try {
+
+        const { email } = req.query;
+        const { userId } = req.params;
+
+        const searchResults = await filterEmailsByAccount(userId, email);
+        res.status(200).json({
+            success: true,
+            message: "Filtered emails retrieved.",
+            searchResults: searchResults
+        })
+        
+    } catch (error) {
+
+        return res.status(500).json({
+            success: false,
+            message: "Error filtering emails. Please try again later.",
+            error: error.message
+        });
+        
+    }
+}
+
+exports.filterEmailsByFolder = async (req, res) => {
+    try {
+        
+        const { folder } = req.query;
+        const { userId } = req.params;
+
+        const searchResults = await filterEmailsByFolder(userId, folder);
+        res.status(200).json({
+            success: true,
+            message: "Filtered emails retrieved.",
+            searchResults: searchResults
+        })
+
+    } catch (error) {
+
+        return res.status(500).json({
+            success: false,
+            message: "Error filtering emails. Please try again later.",
             error: error.message
         });
         
--- routes/elasticSearchRoutes.js
+++ routes/elasticSearchRoutes.js
@@ -4,5 +4,7 @@ const elasticSearchControllers = require('../controllers/elasticSearchController
 
 // Route to search for emails by query for a userId
 router.post('/search/:userId', elasticSearchControllers.searchEmailsByQuery);
+// Route to search for emails by date range for a userId
+router.post('/search/date/range/:userId', elasticSearchControllers.searchEmailsByDateRange);
 
 module.exports = router;
