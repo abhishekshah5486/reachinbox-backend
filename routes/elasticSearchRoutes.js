55c02bed25f6b4af0f255486748bae0f4c5fa3a5
abhishekshah5486 <abhishek.shah5486@gmail.com>
2025-03-05 14:45:38 +0530

    routes to filter emails by folder and account

--- routes/elasticSearchRoutes.js
+++ routes/elasticSearchRoutes.js
@@ -6,5 +6,11 @@ const elasticSearchControllers = require('../controllers/elasticSearchController
 router.post('/search/:userId', elasticSearchControllers.searchEmailsByQuery);
 // Route to search for emails by date range for a userId
 router.post('/search/date/range/:userId', elasticSearchControllers.searchEmailsByDateRange);
+// Route to filter emails by folder for a userId
+router.post('/filter/folder/:userId', elasticSearchControllers.filterEmailsByFolder);
+// Route to filter emails by account for a userId
+router.post('/search/account/:userId', elasticSearchControllers.filterEmailsByAccount);
+// Route to filter emails by folder and account for a userId
+router.post('/filter/folder/account/:userId', elasticSearchControllers.filterEmailsByFolderAndAccount);
 
 module.exports = router;
