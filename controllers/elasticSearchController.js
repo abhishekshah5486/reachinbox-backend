b06f8c0c4ca3ffb9e12d29e8c5dad9adcfc45416
abhishekshah5486 <abhishek.shah5486@gmail.com>
2025-03-05 21:32:12 +0530

    controller to delete emails from elastic search

--- controllers/elasticSearchController.js
+++ controllers/elasticSearchController.js
@@ -1,4 +1,4 @@
-const { searchEmailsByQuery, searchEmailsByDateRange,  filterEmailsByFolderAndAccount, filterEmailsByAccount, filterEmailsByFolder} = require('../services/elasticService');
+const { searchEmailsByQuery, searchEmailsByDateRange,  filterEmailsByFolderAndAccount, filterEmailsByAccount, filterEmailsByFolder, deleteEmailsFromElasticSearchByUserId} = require('../services/elasticService');
 
 exports.searchEmailsByQuery = async (req, res) => {
     try {
@@ -118,3 +118,24 @@ exports.filterEmailsByFolder = async (req, res) => {
 }
 
 // Delete emails for a specific user from elastic search
+const deleteEmailsFromElasticSearchByUserId = async (req, res) => {
+    try {
+
+        const { userId } = req.params;
+        const result = await deleteEmailsFromElasticSearchByUserId(userId);
+        return res.status(200).json({
+            success: true,
+            message: "Emails deleted successfully.",
+            data: result
+        });
+
+    } catch (error) {
+
+        return res.status(500).json({
+            success: false,
+            message: "Error deleting emails. Please try again later.",
+            error: error.message
+        });
+        
+    }
+}
